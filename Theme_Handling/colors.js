99d8d876fff3878a263efb426d4aaf7e4972e98c
sresri2 <125845681+sresri2@users.noreply.github.com>
2024-04-13 21:47:50 -0700

    Add files via upload

---
+++ Theme_Handling/colors.js
@@ -0,0 +1,22 @@
+export const lightTheme={
+    primary: 'white',
+    text: 'black',
+    activeLabel: '#ffe8e8',
+    eqButtonBg: '#d4ffd4',
+    eqButtonText: '#039C00',
+    wfButtonBg: '#faede8',
+    wfButtonText: '#FF5621',
+    rsButtonBG: '#f0f0f0',
+    rsButtonText: '#4d4d4d'
+}
+export const darkTheme={
+    primary: '#242430',
+    text: 'white',
+    activeLabel: '#694242',
+    eqButtonBg: 'rgba(25, 97, 55, 0.4)',
+    eqButtonText: '#02d65b',
+    wfButtonBg: 'rgba(163, 38, 0, 0.4)',
+    wfButtonText: '#ff6e40',
+    rsButtonBG: 'rgba(77, 77, 77, 0.4)',
+    rsButtonText: '#bfbfbf'
+}
